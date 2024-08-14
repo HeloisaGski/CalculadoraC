d05a124f7ca6f967d2b7fd0d1f085ec2e3a5cd36
HeloisaGski <146874643+HeloisaGski@users.noreply.github.com>
2024-08-14 00:08:03 -0300

    Atualizando readme

---
+++ Aula3/Program.cs
@@ -0,0 +1,40 @@
+﻿// //Tipos de variaveis
+// int x = 10;
+// double pi = 3.1415;
+// char letra = 'M';
+// bool isTrue = true;
+// string texto = "Uma linda mensagem";
+    
+// double outroValor;
+// //conversao implicita
+// outroValor = x;
+// //conversao explicita
+// int valorInteiro = (int) pi;
+    
+// //conversao com a classe Convert 
+// int y =Convert.ToInt32(texto);
+// //Operadores
+// //+,-,/,*, = , ==
+// int numero;
+// Console.WriteLine("Digite um numero:");
+// numero = Convert.ToInt32(Console.ReadLine());
+// //concatenação de strings
+// Console.WriteLine("O usuario digitou: " + numero);
+// //Interpolação de strings
+// Console.WriteLine($"O dobro do que o usuário digitou é : {numero*2}");
+
+        Console.WriteLine("Digite o primeiro número para calcular: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número para calcular: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        double soma = numero1 + numero2;
+        double subtracao = numero1 - numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao = numero1 / numero2;
+
+        Console.WriteLine("O valor da soma dos seus números é: " + soma);
+        Console.WriteLine($"O valor da subtração dos seus números é: {subtracao}");
+        Console.WriteLine(string.Format("A subtração dos números é igual {0}", multiplicacao));
+        Console.WriteLine(string.Format("A divisão dos números é igual {0}", divisao));
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CalculadoraC
- Calculadora em C# desenvolvida durante aula de laboratório de programação
+ Calculadora em C# desenvolvida durante aula 03 de laboratório de programação, com os seguintes requisitos:
+ 
+Desenvolvimento de um Programa Completo: Calculadora e Formatação de Resultados
+Neste exercício, os alunos irão desenvolver uma calculadora simples que realiza as quatro operações básicas (adição, subtração, multiplicação e divisão), utilizando operadores aritméticos e formatando os resultados utilizando os conceitos de concatenação, interpolação e placeholders.
+
+Passo a passo:
+Entrada de Dados:
+Peça ao usuário para inserir o primeiro número.
+Peça ao usuário para inserir o segundo número.
+Processamento:
+Realize cada uma das operações matemáticas básicas (adição, subtração, multiplicação e divisão) utilizando os dois números fornecidos pelo usuário.
+Exibição dos Resultados:
+Exiba os resultados das quatro operações, formatando-os de diferentes maneiras:
+Usando concatenação para uma das operações.
+Usando interpolação para outra.
+Usando placeholders para as restantes.
